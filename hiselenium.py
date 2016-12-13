6b2a66d5fc504d0af6bcd1530653059b88957dc7
elizabethrussell <ecrussel93@gmail.com>
2016-12-12 21:31:04 -0600

    oh and click babe

--- hiselenium.py
+++ hiselenium.py
@@ -6,6 +6,13 @@ driver = webdriver.Chrome()
 driver.get(url)
 driver.get(url) #with cookies
 
+
 subjectDropDown = driver.find_element_by_xpath('//*[@id="SSR_CLSRCH_WRK_ACAD_ORG$2"]')
 for option in subjectDropDown.find_elements_by_tag_name('option'):
-	print option.text
+	if not option.text.isspace():
+		option.click()
+		submitButton = driver.find_element_by_xpath('//*[@id="CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH"]')
+		submitButton.click()
+		break
+
+
